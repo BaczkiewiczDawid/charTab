7b755d1ea524176d26003bd3137e2e97ed743c99
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-05-12 17:35:04 +0200

    basic UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Table } from "@/components/table/table"
 import { translations } from "@/app/data/lang"
+import { data } from "@/app/data/dummyData"
 
 export default function Home() {
 
@@ -9,8 +10,10 @@ export default function Home() {
     <div className={"flex justify-center p-4 text-white"}>
       <div className={"w-screen text-white h-screen overflow-auto"}>
         <Table
+          data={data}
           lang={"pl"}
           translations={translations}
+          ableToDelete
         />
       </div>
     </div>

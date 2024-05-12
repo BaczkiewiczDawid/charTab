7bc9a3b8c7b7ca05d51bb28fde4283835607dd5c
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-05-12 20:25:35 +0200

    button to delete item

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -2,7 +2,7 @@
 
 import {Table as TableComponent, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import {Translations} from "@/app/types/translations";
-import {Ellipsis} from "lucide-react";
+import {Ellipsis, Trash} from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -28,18 +28,19 @@ export const Table = ({data, lang, translations, ableToDelete}: TableProps) => {
   return (
     <TableComponent className={"border border-gray-600"}>
       <TableHeader className={"bg-stone-900"}>
-        <TableRow>
+        <TableRow className={"border-stone-900"}>
           {Object.keys(data[0]).map((key, index) => {
             const keyToTranslate = selectedTranslations?.general && (key as keyof typeof selectedTranslations.general) in selectedTranslations.general
               ? selectedTranslations.general[key as keyof typeof selectedTranslations.general]
               : key
 
-              return (
-               <TableHead key={index}>
-                 {keyToTranslate}
-               </TableHead>
-              )
+            return (
+              <TableHead key={index}>
+                {keyToTranslate}
+              </TableHead>
+            )
           })}
+          {ableToDelete && <TableHead></TableHead>}
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -47,7 +48,7 @@ export const Table = ({data, lang, translations, ableToDelete}: TableProps) => {
           const isOdd = index % 2 === 0
 
           return (
-            <TableRow key={index}  className={`${isOdd ? 'bg-stone-950' : 'bg-stone-900'}`}>
+            <TableRow key={index} className={`${isOdd ? 'bg-stone-950' : 'bg-stone-900'}`}>
               {Object.values(row).map((value, index) => {
                 return (
                   <TableCell key={index} className={"border border-gray-600"}>
@@ -55,6 +56,25 @@ export const Table = ({data, lang, translations, ableToDelete}: TableProps) => {
                   </TableCell>
                 )
               })}
+              {ableToDelete &&
+                  <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                          <TableCell className={"border border-gray-600 text-center"}>
+                              <div className={"inline-block m-auto cursor-pointer"}><Ellipsis/></div>
+                          </TableCell>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                          <DropdownMenuGroup>
+                            {ableToDelete &&
+                                <DropdownMenuItem className={"flex items-center text-xs cursor-pointer"}>
+                                    <Trash size={16} strokeWidth={2}/>
+                                    <span className={"ml-2"}>Delete</span>
+                                </DropdownMenuItem>
+                            }
+                          </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                  </DropdownMenu>
+              }
             </TableRow>
           )
         })}

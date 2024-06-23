9f7f0744898aea33b69cf909476158b92cd58157
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-06-23 17:43:55 +0200

    import and transform CSV data

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ import { Table } from "@/components/table/table";
 import { translations } from "@/data/lang";
 import { Navigation } from "@/components/navigation";
 import { TableProvider, useTableContext } from "@/context/table-context";
+import { ImportCSV } from "@/components/import-csv"
 
 const HomeContent = () => {
   const { ableToDelete, showAlerts, multipleChoiceFilter, columnsToFilter, columnsOrder, dataToRender, columnsToSum, columnsToHide } = useTableContext();
 
   return (
     <div className={"flex flex-col"}>
-      <div className="w-full p-4">Header</div>
+      <div className="w-full p-4 flex justify-between">
+        <p>Header</p>
+        <ImportCSV />
+      </div>
       <div className={"grid grid-cols-4 w-full mt-4"}>
         <nav className={"p-4 col-span-1"}>
           <Navigation />
